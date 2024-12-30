389891bfbeba77ccaba7086696f6b1aa546940cb
xRubby <rubengigante7@gmail.com>
2024-12-30 16:30:07 +0100

    Alcune sistemazioni

--- src/test/java/it/unisa/magazon_lab/DAO/GestioneListeDAOTest.java
+++ src/test/java/it/unisa/magazon_lab/DAO/GestioneListeDAOTest.java
@@ -5,16 +5,28 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
+/**
+ * Classe di test per verificare i casi di validazione del metodo inserisciLista di GestioneListeDAO.
+ * @author Gigante Ruben
+ */
+
 public class GestioneListeDAOTest {
 
     private GestioneListeDAO gestioneListeDAO;
 
+
+    /**
+     * Configura l'istanza di GestioneListeDAO prima di ogni test.
+     */
     @BeforeEach
     public void setUp() {
         gestioneListeDAO = GestioneListeDAO.getInstance();
     }
 
-
+    /**
+     * TC_5.1.1
+     * Formato file non valido.
+     */
     @Test
     public void T_5_1_1(){
 
@@ -23,11 +35,15 @@ public class GestioneListeDAOTest {
         try{
             gestioneListeDAO.inserisciLista(nomeFile);
         }catch(Exception e){
-            assertEquals("nomeFile non rispetta il formato", e.getMessage());
+            assertEquals("nomeFile non rispetta il formato", e.getMessage(), "Errore: Formato file non valido.");
         }
 
     }
 
+    /**
+     * TC_5.1.2
+     * Formato file corretto.
+     */
 
     @Test
     public void T_5_1_2(){
